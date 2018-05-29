df9bdaded9fe5a218940e3d631af67a9f5d9d26a
Rohit Kumar <rohit9075@gmail.com>
2018-05-29 12:46:04 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/android/dialogs/DataEntryDialog.java
@@ -0,0 +1,43 @@
+package com.rohit.android.dialogs;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class DataEntryDialog  extends DialogFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.data_entry_dialog, container,false);
+
+        Button btnOk = rootView.findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+
+        Button btnCancel = rootView.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return rootView;
+
+    }
+}
